67dbdc098ba85e4d8818c6338ec44615da084e66
Chris Underwood <cunderwoodmn@gmail.com>
2022-06-16 19:41:56 -0700

    update 1st test

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -3,6 +3,22 @@ import { test, it, expect } from 'vitest';
 import { add } from './math';
 
 it('should summrize all numbers in an array', () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  //Arrange
+  const numbers = [1, 2, 3, 44];
+
+  const expectedResult = numbers.reduce((prevVal, currVal) => {
+    return prevVal + currVal;
+  }, 0);
+  //Act
+  const result = add(numbers);
+  //Assert
+  expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if at least one invalid number is provided', () => {
+  const inputs1 = ['invalid', 55];
+
+  const results1 = add(inputs1);
+
+  expect(results1).toBe(NaN);
 });
