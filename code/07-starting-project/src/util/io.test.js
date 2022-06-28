6e8aa79d0a994281abf1a5d31bdf8d5718b2db10
Chris Underwood <cunderwoodmn@gmail.com>
2022-06-28 14:39:47 -0700

    deleted cats.textand implemented a mock for fs.writefile

--- code/07-starting-project/src/util/io.test.js
+++ code/07-starting-project/src/util/io.test.js
@@ -1,9 +1,16 @@
-import { it, expect } from 'vitest';
+import { it, expect, vi } from 'vitest';
+import { promises as fs } from 'fs';
 
 import writeData from './io';
+
+vi.mock('fs');
 
 it('should execute the writefile method', () => {
   const data = 'Smithson';
   const filename = 'cats.txt';
-  return expect(writeData(data, filename)).resolves.toBeUndefined();
+
+  writeData(data, filename);
+
+  //return expect(writeData(data, filename)).resolves.toBeUndefined();
+  return expect(fs.writeFile).toBeCalled();
 });
