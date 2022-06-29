7f3a3dbdb69dc1c54ecf2d112e1d22ded26e448f
Chris Underwood <cunderwoodmn@gmail.com>
2022-06-28 18:09:06 -0700

    add custom mock code for 'api' in io.test.js

--- code/07-starting-project/src/util/io.test.js
+++ code/07-starting-project/src/util/io.test.js
@@ -6,6 +6,15 @@ import writeData from './io';
 // Create a mock fs so that when writeData is called, it will use the test file system and not the real one.
 // This is done so that no side effects are caused such as writing to cats.txt
 vi.mock('fs');
+vi.mock('path', () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
 
 it('should execute the writefile method', () => {
   const data = 'Smithson';
@@ -14,5 +23,6 @@ it('should execute the writefile method', () => {
   writeData(data, filename);
 
   //return expect(writeData(data, filename)).resolves.toBeUndefined();
-  return expect(fs.writeFile).toBeCalled();
+  //return expect(fs.writeFile).toBeCalled();
+  return expect(fs.writeFile).toBeCalledWith(filename, data);
 });
