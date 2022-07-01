218f93c009c6db12886e06d15d2cc124e0924556
Chris Underwood <cunderwoodmn@gmail.com>
2022-06-30 20:33:31 -0700

    mock fetch globla function

---
+++ code/08-starting-project/util/https.test.js
@@ -0,0 +1,27 @@
+import { it, expect, describe, vi } from 'vitest';
+
+import { sendDataRequest } from './http';
+
+const testResponseData = { testKey: 'testData' };
+const testFetch = vi.fn((url, options) => {
+  return new Promise((resolve, reject) => {
+    const testResponse = {
+      ok: true,
+      json: () => {
+        return new Promise((resolve, reject) => {
+          resolve(testResponseData);
+        });
+      },
+    };
+    resolve(testResponse);
+  });
+});
+
+vi.stubGlobal('fetch', testFetch);
+
+describe('sendDataRequest()', () => {
+  it('should return any available respnose data', () => {
+    const testData = { key: 'test' };
+    return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+  });
+});
