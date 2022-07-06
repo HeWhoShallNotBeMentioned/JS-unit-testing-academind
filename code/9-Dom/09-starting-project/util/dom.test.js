d4597b46d2fd63997e1c372d1df38f53bb8e6e3b
Chris Underwood <cunderwoodmn@gmail.com>
2022-07-05 17:13:31 -0700

    add the index.html document to be visible to vitest

--- code/9-Dom/09-starting-project/util/dom.test.js
+++ code/9-Dom/09-starting-project/util/dom.test.js
@@ -1,5 +1,15 @@
-import { it, expect, describe } from 'vitest';
+import fs from 'fs';
+import path from 'path';
+import { it, expect, describe, vi } from 'vitest';
+import { Window } from 'happy-dom';
 import { showError } from './dom';
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDOcumentContent = fs.readFileSync(htmlDocPath).toString();
+const window = new Window();
+const document = window.document;
+document.write(htmlDOcumentContent);
+vi.stubGlobal('document', document);
 
 it('first test of the dom', () => {
   showError('test');
